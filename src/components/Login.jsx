ab536127d7e7281a56b6400a797543036195ead7
akrhode <a.rhode@web.de>
2023-04-24 21:26:50 +0200

    Test and build input fields & button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
-import axios from "axios";
-
 const Login = () => {
   return (
     <div className="container">
-      <form></form>
+      <form>
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+        <button>Login</button>
+      </form>
     </div>
   );
 };
