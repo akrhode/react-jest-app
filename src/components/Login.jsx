18182dc09cd315d43a7046dc431d477a9e1407a8
akrhode <a.rhode@web.de>
2023-04-24 21:55:20 +0200

    Test and make error invisible

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [error, setError] = useState(false);
   return (
     <div className="container">
       <form>
         <input type="text" placeholder="username"></input>
         <input type="password" placeholder="password"></input>
         <button disabled="true">Login</button>
+        <span
+          data-testid="error"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
       </form>
     </div>
   );
