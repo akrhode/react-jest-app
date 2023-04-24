8618129f3728d22129af8caa322a04e315a80fae
akrhode <a.rhode@web.de>
2023-04-24 22:45:35 +0200

    Test and add loading to button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,30 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      setUser(data);
+    } catch {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="container">
+      <span className="user">{user.name}</span>
       <form>
         <input
           type="text"
@@ -19,7 +38,9 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button disabled={!username || !password}>Login</button>
+        <button disabled={!username || !password} onClick={handleClick}>
+          {loading ? "please wait" : "Login"}
+        </button>
         <span
           data-testid="error"
           style={{ visibility: error ? "visible" : "hidden" }}
